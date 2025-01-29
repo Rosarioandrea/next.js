56ed0f0125ef3774fbecec5aa6f3a19d4e48222a
Rosarioandrea <rosarioandreamartinez@gmail.com>
2025-01-29 13:29:27 -0300

    correcciones pagina not found

--- app/not-found.js
+++ app/not-found.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 const NotFoundPage = () => {
     return (
     <div className="flex flex-col items-center space-y-8">
-        <h1 className="text-4xl font-bold text-center text-primary">
+        <h1 className="text-4xl font-bold text-center text-black">
         Pagina no encontrada.
         </h1>
         <Link
-        href="/" className="p-4 bg-secondary/70 shadow-md rounded-sm text-white hover:bg-secondary/90 transition-colors">
+        href="/" className="p-4 bg-black/90 shadow-md rounded-sm text-white hover:bg-black/20 transition-colors">
         Volver al Home
         </Link>
     </div>
